e26c62aa788b9c8e6f65920156fe6b07fff664f2
Nguyen, Victor <22437776+victornguyen75@users.noreply.github.com>
2021-03-29 16:06:11 -0500

    add a board with a snake on it

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { LinkedListNode, SinglyLinkedList } from "./LinkedList";
 import "./Board.css";
 
-export default function Board() {
-  return <p>Board!</p>;
-}
+const BOARD_SIZE = 10;
+
+const Board = () => {
+  const [board, setBoard] = useState(createBoard(BOARD_SIZE));
+  const [snakeCells, setSnakeCells] = useState(new Set([44]));
+  const [snake, setSnake] = useState(new SinglyLinkedList(44));
+
+  return (
+    <div className="board">
+      {board.map((row, rowIndex) => (
+        <div key={rowIndex} className="row">
+          {row.map((cellValue, cellIndex) => (
+            <div
+              key={cellIndex}
+              className={`cell ${
+                snakeCells.has(cellValue) ? "cell-green" : ""
+              }`}
+            ></div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const createBoard = (BOARD_SIZE) => {
+  let counter = 1;
+  const board = [];
+  for (let row = 0; row < BOARD_SIZE; row++) {
+    const currentRow = [];
+    for (let col = 0; col < BOARD_SIZE; col++) {
+      currentRow.push(counter++);
+    }
+    board.push(currentRow);
+  }
+  return board;
+};
+
+export default Board;
